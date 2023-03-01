e82a33a4ee13b4faf1c25d51831bc1d254ab4d67
vermashiva12 <115514079+vermashiva12@users.noreply.github.com>
2023-02-28 19:11:43 -0500

    No Logo in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ const Footer = () => {
   return (
     <div className='w-full h-[120px] justify-between items-start px-4 bg-gray-800/90 text-gray-100'>
         
-     
+{/*      
             <img src={Logo} alt='Logo' style={{width: '20px'}} />
-        
+         */}
             <p className=' py-6 text-right text-[11px]'>Copyright @ 2023 Humble Glorious Glass. Rights Reserved </p>
-            <p className='text-right  text-[9px]'>Desgined by Shivanshu </p>
+            <p className='text-right  text-[9px]'>Desgined by Shivanshu! </p>
         
     </div>
   )
